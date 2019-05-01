6ed6f9728622dd1cccc945cbb14c41728318b798
Or Shelly <or@magisto.com>
2019-05-01 10:32:14 +0300

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -15,7 +15,7 @@ class  QuizQuestion extends Component {
                 <section className="buttons">
                     <ul>
                         {this.props.quiz_question.answer_options.map((answer_options, index) =>
-                            <QuizQuestionButton button_text={answer_options} key={index} clickHandler ={this.handleClick.bind(this)}/>
+                            <QuizQuestionButton button_text={answer_options} key={index} clickHandler={this.handleClick.bind(this)}/>
                         )}
                     </ul>
                 </section>
