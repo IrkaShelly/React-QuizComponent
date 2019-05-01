7dc8dc9f864023fedd95d06ca167237a3357bc48
Or Shelly <or@magisto.com>
2019-05-01 10:23:06 +0300

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 class  QuizQuestion extends Component {
+    handleClick = (buttonText) =>{
+        if (buttonText === this.props.quiz_question.answer)
+            this.props.showNextQuestionHandler();
+    }
     render() {
         return (
             <main>
@@ -9,7 +13,9 @@ class  QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                        {this.props.quiz_question.answer_options.map((answer_options, index) =>
+                            <QuizQuestionButton button_text={answer_options} key={index} clickHandler ={this.handleClick.bind(this)}/>
+                        )}
                     </ul>
                 </section>
             </main>
