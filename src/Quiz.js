6f77446a71cd3c573ed9108bc80d71bf1c8f94aa
Or Shelly <or@magisto.com>
2019-05-01 10:51:55 +0300

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,9 @@ class Quiz extends Component {
     showNextQuestion() {
          this.setState((state)=> {return {quiz_position: state.quiz_position+1};});
     }
-
+    handleResetClick() {
+        this.state = {quiz_position:1};
+    }
     render() {
         const isQuizEnd = this.state.quiz_position -1 === quizData.quiz_questions.length;
         return (
@@ -20,7 +22,8 @@ class Quiz extends Component {
                 { isQuizEnd ?
                     <QuizEnd/>
                     : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-                                    showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
+                                    showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                                    resetClickHandler={this.handleResetClick.bind(this)}/>
                 }
             </div>
         );
